b2e91d6d93ff0b9e34756e01a7dbdf1e60011975
soenkemoeller <soenkeamoeller@gmail.com>
2013-02-05 18:23:28 -0800

    SWEEP_CONTROL UI almost completed, still needs update commands
    Still needs onChange style update methods, but most of the GUI
    interface is finished.

--- clients/SWEEP_CONTROL.py
+++ clients/SWEEP_CONTROL.py
@@ -1,12 +1,17 @@
 import os
 
 from PsQt3 import QtGui
-from PyQt4 import QtCore
 from twisted.internet.defer import inlineCallbacks, returnValue
 
 from labrad.wrapper import connectAsync
 from labrad.types import Error
 from labrad import types as T                                   ## obfuscation
+
+FREQ_MIN
+FREQ_MAX
+SWEEP_RANGE_STEP = 0.05
+SWEEP_STEP_STEP = 0.0
+SWEEP_TIME_STEP
 
 
 class SWEEP_WIDGET(QTGui.QWidget):
@@ -21,26 +26,54 @@ class SWEEP_WIDGET(QTGui.QWidget):
         groupbox = QtGui.QGroupBox('Sweep Control (Marconi)')
         groupboxLayout = QtGui.QGridLayout()
 
-        # build controls
-        # ...
-        carrierModeButton = QtQui.QPushButton() # change FIXED or SWEPT
-        sweepRangeStartCtrl = QtGui.QDoubleSpinBox() # change sweep start freq
-        sweepRangeStopCtrl = QtGui.QDoubleSpinBox() # change sweep stop freq
-        sweepStepCtrl = QtGui.QDoubleSpinBox()      # change sweep step
-        sweepTimeCtrl = QtGui.QDoubleSpinBox()      # change sweep time/step
-        #sweepTrigModeToggle    # make a button that toggles 4 values
-        sweepBeginButton = QtGui.QPushButton()      # start a sweep
-        sweepPauseButton = QtGui.QPushButton()      # pause the sweep
-        sweepContinueButton = QtGui.QPushBotton()   # continue after pause
-        sweepResetButton = QtGui.QPushButton()      # reset the sweep freq
+        # build controls and put in sublayouts
+        # topLayout
+        self.carrierModeButton = QtQui.QPushButton() # change FIXED or SWEPT
+        self.sweepTrigModeToggle = QtGui.QPushButton     # make it a menu button 
+        topLayout = QtGui.QGridLayout()
+        topLayout.addWidget(self.carrierModeButton, 0, 0)
+        topLayout.addWidget(self.sweepTrigModeToggle, 0, 1)
+
+        # upperMiddleLayout
+        self.sweepRangeStartCtrl = QtGui.QDoubleSpinBox() # change sweep start freq
+        self.sweepRangeStopCtrl = QtGui.QDoubleSpinBox() # change sweep stop freq
+        upperMiddleLayout = QtGui.QGridLayout()
+        upperMiddleLayout.addWidget(self.sweepRangeStartCtrl, 0, 0)
+        upperMiddleLayout.addWidget(self.sweepRangeStopCtrl, 0, 1)
         
-        # NOTE: QtGui.QGridLayout has an addLayout method, so you can
-        # have a hierarchical organization to make things go where you want
+        # lowerMiddleLayout
+        self.sweepStepCtrl = QtGui.QDoubleSpinBox()      # change sweep step
+        #self.sweepStepCtrl.setRange(
+        self.sweepStepCtrl.setDecimals(5)
+        self.sweepStepCtrl.setSingleStep(0.05)
+        self.sweepTimeCtrl = QtGui.QDoubleSpinBox()      # change sweep time/step
+        #self.sweepTimeCtrl.setRange(
+        self.sweepTimeCtrl.setDecimals(1)
+        self.sweepTimeCtrl.setSingleStep(10)
+        lowerMiddleLayout = QtGui.QGridLayout()
+        lowerMiddleLayout.addWidget(QtGui.QLabel('Sweep Step [MHz]'), 0, 0)
+        lowerMiddleLayout.addWidget(self.sweepStepCtrl, 1, 0)
+        lowerMiddleLayout.addWidget(QtGui.QLabel('Time/Step [ms]'), 0, 1)
+        lowerMiddleLayout.addWidget(self.sweepTimeCtrl, 1, 1)
+
+        # bottomLayout
+        self.sweepBeginButton = QtGui.QPushButton()      # start a sweep
+        self.sweepPauseButton = QtGui.QPushButton()      # pause the sweep
+        self.sweepContinueButton = QtGui.QPushBotton()   # continue after pause
+        self.sweepResetButton = QtGui.QPushButton()      # reset the sweep freq
+        bottomLayout = QtGui.QGridLayout()
+        bottomLayout.addWidget(sweepBeginButton)
+        bottomLayout.addWidget(sweepPauseButton)
+        bottomLayout.addWidget(sweepContinueButton)
+        bottomLayout.addWidget(sweepResetButton)
 
         # add them to the groupboxLayout
-        # ...
-        # groupboxLayout.addWidget(ctrl, 0, 0) ...
-
+        groupboxLayout.addLayout(topLayout)
+        groupboxLayout.addLayout(upperMiddleLayout)
+        groupboxLayout.addLayout(lowerMiddleLayout)
+        groupboxLayout.addLayout(bottomLayout)
+        
+        # everything goes in the mainLayout
         groupbox.setLayout(groupboxLayout) # set the groupboxLayout in the groupbox
         mainLayout.addWidget(groupbox) # put the groupbox in the mainLayout
         self.setLayout(mainLayout) # set this widget's main layout
