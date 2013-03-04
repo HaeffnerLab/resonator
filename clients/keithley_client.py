b83104f3a8dd65ecc0ebaadef0d2ab98f7de13c9
Ryoh Masuda <ryohmasuda@berkeley.edu>
2013-03-04 11:33:43 -0800

    Second version of keithley client

--- clients/keithley_client.py
+++ clients/keithley_client.py
@@ -6,13 +6,21 @@ import voltage_conversion as cF
 cxn = labrad.connect()
 kdmm = cxn.Keithley_2100 _DMM()
 
+
+
+filename='c:/data_resonator/keithley_DMM_'+time.strftime("%d%m%Y_%H%M")+'.csv'
+numpy.savetxt(filename,time.strftime("%H%M"),voltage,tempK)
+
 while(1):
     voltage=[]
     tempK=[]
     voltage_array = kdmm.getdcVolts().asarray
-    voltage.append(kdmm.getdcVolts())
+    #voltage.append(kdmm.getdcVolts())
     tempK_array= cF.converter(kdmm.getdcVolts()).asarray
-    tempK.append(cF.converter(kdmm.getdcVolts())
-    filename="C:/Documents/Desktop/Data_DMM/Voltage vs Temperature.csv"
-    numpy.savetxt(filename,tempK)
+    #tempK.append(cF.converter(kdmm.getdcVolts())
     #time.sleep(60)
+    
+f.open(filename,"w")
+f.write("%d%m%Y_%H%M",voltage_array,tempK_array)
+f.close()
+    
