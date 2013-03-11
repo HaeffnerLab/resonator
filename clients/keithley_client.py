7673daaef9b9265b99d0db8b18152d4deabf676b
ryohmasuda <ryohmasuda@berkeley.edu>
2013-03-11 11:11:30 -0700

    Update keithley_client.py

--- clients/keithley_client.py
+++ clients/keithley_client.py
@@ -1,22 +1,25 @@
 import labrad
 import numpy
 import time
-import voltage_conversion
+from voltage_conversion import voltage_conversion as VC
 import csv
-VC=voltage_conversion()
+#VC=voltage_conversion
 
 cxn = labrad.connect()
-kdmm = cxn.Keithley_2100_DMM()
+kdmm = cxn.keithley_2100_dmm()
 kdmm.select_device()
 
 filedirectly='c:/data_resonator_voltage/keithley_DMM_'+time.strftime("%d%m%Y_%H%M")+'.csv'
 filename=open(filedirectly,"wb")
 fcsv=csv.writer(filename,lineterminator="\n")
 t=time.strftime("%H%M")
+
+vc = VC()
+
 while(1):
-    v=kdmm.getdcVolts()
-    tempK=VC.converter(v)
-    fcsv.writerow([t,v,temp])
+    voltage = kdmm.get_dc_volts()
+    tempK=vc.conversion(voltage)
+    fcsv.writerow([t,voltage,tempK])
     
 
     #time.sleep(60)
