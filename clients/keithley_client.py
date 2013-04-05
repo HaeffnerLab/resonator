5e3b4f0c9377717a38274040e5d47d1dc0831bcd
ryohmasuda <ryohmasuda@berkeley.edu>
2013-04-05 15:51:20 -0700

    Update keithley_client.py

--- clients/keithley_client.py
+++ clients/keithley_client.py
@@ -14,12 +14,12 @@ filedirectly_529='c:/data_resonator_voltage/BNC529_keithley_DMM_'+run_time+'.csv
 
 file_526=open(filedirectly_526,"W")
 fcsv_526=csv.writer(file_526,lineterminator="\n")
-fcsv_526.writerow(["time (Hour:Minute)", "voltage(V)",  "temperature(K)"])
+fcsv_526.writerow(["time (Hour:Minute)", "voltage(V)", "temperature(K)"])
 file_526.close()
 
 file_529=open(filedirectly_529,"W")
 fcsv_529=csv.writer(file_529,lineterminator="\n")
-fcsv_529.writerow(["time(Hour:Minute)", "voltage(V)",  "temperature(K)"])
+fcsv_529.writerow(["time(Hour:Minute)", "voltage(V)", "temperature(K)"])
 file_529.close()
 
 vc = VC()
@@ -33,7 +33,7 @@ while(1):
     file_526.close()
     time.sleep(30)
 
-    filen_529=open(filedirectly529,"a")
+    file_529=open(filedirectly_529,"a")
     fcsv_529=csv.writer(file_529,lineterminator="\n")
     voltage = kdmm.get_dc_volts()
     tempK=vc.conversion(voltage)
