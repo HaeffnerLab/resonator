f2b39d9bf2eb2342074eb75934627b9e7cf90139
ryohmasuda <ryohmasuda@berkeley.edu>
2013-04-15 10:19:00 -0700

    Update keithley_client.py

--- clients/keithley_client.py
+++ clients/keithley_client.py
@@ -27,14 +27,14 @@ file_529.close()
 
 vc = VC()
 while(1):
-    file_526=open(filedirectory_526,"ab")
-    fcsv_526=csv.writer(file_526,lineterminator="\n")
-    voltage = keithley.get_dc_volts()
-    tempK=vc.conversion(voltage)
-    elapsed_time_526 = (time.time() - initial_time)/60
-    fcsv_526.writerow([elapsed_time_526,time.strftime("%H"+":"+"%M"),voltage,tempK])
-    file_526.close()
-    time.sleep(30)
+#    file_526=open(filedirectory_526,"ab")
+#    fcsv_526=csv.writer(file_526,lineterminator="\n")
+#    voltage = keithley.get_dc_volts()
+#    tempK=vc.conversion(voltage)
+#    elapsed_time_526 = (time.time() - initial_time)/60
+#    fcsv_526.writerow([elapsed_time_526,time.strftime("%H"+":"+"%M"),voltage,tempK])
+#    file_526.close()
+#    time.sleep(30)
 
     file_529 = open(filedirectory_529,"ab")
     fcsv_529 = csv.writer(file_529,lineterminator="\n")
@@ -43,4 +43,5 @@ while(1):
     elapsed_time_529 = (time.time() - initial_time)/60
     fcsv_529.writerow([elapsed_time_529,time.strftime("%H"+":"+"%M"),voltage,tempK])
     file_529.close()
-    time.sleep(30)
+    #Don't forget to change this time to 30 next time! 
+    time.sleep(60)
