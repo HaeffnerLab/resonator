50ad064fa54a89695dc840748e0029049ce3ab9a
ryohmasuda <ryohmasuda@berkeley.edu>
2013-04-15 08:59:47 -0700

    Update keithley_client.py

--- clients/keithley_client.py
+++ clients/keithley_client.py
@@ -17,12 +17,14 @@ filedirectly_529 ='c:/data_resonator_voltage/keithley_DMM_'+run_time+'/529(Insid
 
 file_526 = open(filedirectly_526,"wb")
 fcsv_526 = csv.writer(file_526,lineterminator="\n")
-fcsv_526.writerow(["time passed (minutes)", "current time(H:M)", "voltage(V)", "temperature(K)"])
+//label the each column
+fcsv_526.writerow(["Elapsed Time (Minutes)", "Current Time(H:M)", "Voltage(V)", "Temperature(K)"])
 file_526.close()
 
 file_529 = open(filedirectly_529,"wb")
 fcsv_529 = csv.writer(file_529,lineterminator="\n")
-fcsv_529.writerow(["time passed (minutes)", "current time(H:M)", "voltage(V)", "temperature(K)"])
+//label the each column
+fcsv_529.writerow(["Elapsed Time (Minutes)", "Current Time(H:M)", "Voltage(V)", "Temperature(K)"])
 file_529.close()
 
 vc = VC()
@@ -31,8 +33,8 @@ while(1):
     fcsv_526=csv.writer(file_526,lineterminator="\n")
     voltage = kdmm.get_dc_volts()
     tempK=vc.conversion(voltage)
-    minutes_526 = (time.time() - initial_time)/60
-    fcsv_526.writerow([("%.4f" % current_526),time.strftime("%H"+":"+"%M"),voltage,tempK])
+    elapsed_time_526 = (time.time() - initial_time)/60
+    fcsv_526.writerow([("%.5f" % elapsed_time_526),time.strftime("%H"+":"+"%M"),voltage,tempK])
     file_526.close()
     time.sleep(30)
 
@@ -40,8 +42,8 @@ while(1):
     fcsv_529 = csv.writer(file_529,lineterminator="\n")
     voltage = kdmm.get_dc_volts()
     tempK = vc.conversion(voltage)
-    minutes_529 = (time.time() - initial_time)/60
-    fcsv_529.writerow([("%.4f" % current_529),time.strftime("%H"+":"+"%M"),voltage,tempK])
+    elapsed_time_529 = (time.time() - initial_time)/60
+    fcsv_529.writerow([("%.5f" % elapsed_time_529),time.strftime("%H"+":"+"%M"),voltage,tempK])
     file_529.close()
     time.sleep(30)
 
