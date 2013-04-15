3828a4618f29fa50cd962d3eaa8c1ce35f672520
ryohmasuda <ryohmasuda@berkeley.edu>
2013-04-15 09:22:34 -0700

    Update keithley_client.py

--- clients/keithley_client.py
+++ clients/keithley_client.py
@@ -8,26 +8,31 @@ cxn = labrad.connect()
 keithley = cxn.keithley_2100_dmm()
 keithley.select_device()
 
+#record the running time for file directly
 run_time = time.strftime("%d%m%Y_%H%M")
+#record the initail time for calculating elapsed time (this records in second)
 initial_time = time.time()
+#Define the file directly for both BNCs (526 and 529). They are in the different files in the same folder
 #BNC 526 is at Cold Finger
 filedirectly_526 ='c:/data_resonator_voltage/keithley_DMM_'+run_time+'/526(Cold Finger).csv'
 #BNC 529 is inside the heat shield
 filedirectly_529 ='c:/data_resonator_voltage/keithley_DMM_'+run_time+'/529(Inside Heat Shield).csv'
 
+#First, open each file and note the label for each data 
+#("Time Elapsed","Current Time", "Voltage" and "Temperature")
 file_526 = open(filedirectly_526,"wb")
 fcsv_526 = csv.writer(file_526,lineterminator="\n")
-#label the each column
-fcsv_526.writerow(["Elapsed Time (Minutes)", "Current Time(H:M)", "Voltage(V)", "Temperature(K)"])
+fcsv_526.writerow(["Elapsed Time (minutes)", "Current Time(H:M)", "Voltage(V)", "Temperature(K)"])
 file_526.close()
 
 file_529 = open(filedirectly_529,"wb")
 fcsv_529 = csv.writer(file_529,lineterminator="\n")
-#label the each column
-fcsv_529.writerow(["Elapsed Time (Minutes)", "Current Time(H:M)", "Voltage(V)", "Temperature(K)"])
+fcsv_529.writerow(["Elapsed Time (minutes)", "Current Time(H:M)", "Voltage(V)", "Temperature(K)"])
 file_529.close()
 
+#instantiate Voltage_Conversion in order to convert the measured votlage into Kelvin Temperature
 vc = VC()
+#Run the loop while the device is in service.
 while(1):
     file_526=open(filedirectly_526,"ab")
     fcsv_526=csv.writer(file_526,lineterminator="\n")
@@ -46,5 +51,3 @@ while(1):
     fcsv_529.writerow([("%.5f" % elapsed_time_529),time.strftime("%H"+":"+"%M"),voltage,tempK])
     file_529.close()
     time.sleep(30)
-
-
