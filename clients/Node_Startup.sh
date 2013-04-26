55a40c4395a65f5ae8336f2cfbe703f0c47119c2
soenkemoeller <soenkeamoeller@gmail.com>
2013-04-25 17:34:17 -0700

    Add Node_Startup.sh (quick start node server)
    This script starts the node server and runs NodeClient-control-start.py
    which starts necessary servers.

---
+++ clients/Node_Startup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Start_Node.sh
+# Created: 4/25/13
+# Modified:
+# Author: Ryan Blais
+
+# Script to start node server and use it to start commonly used servers.
+# The LabRAD manager should already be started.
+
+# Start the node server
+source ~/.virtualenvs/labrad/bin/activate
+twistd -n labradnode &
+sleep 2
+
+# Start commonly used servers and/or clients
+python NodeClient-control-start.py
+sleep 2
