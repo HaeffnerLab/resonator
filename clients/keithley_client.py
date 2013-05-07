52fc8bca1e3858796f05d0408803eb45ccbe2020
ryohmasuda <ryohmasuda@berkeley.edu>
2013-05-06 21:19:25 -0700

    change to run on the server from Linux. Also simultaneously connect to keithley dmm on Windows

--- clients/keithley_client.py
+++ clients/keithley_client.py
@@ -1,42 +1,34 @@
 import labrad
-import numpy
 import time
 from voltage_conversion import voltage_conversion as VC
 import csv
 
+
 cxn = labrad.connect()
-keithley = cxn.keithley_2100_dmm()
+pulser = cxn.pulser()
+cxndmm = labrad.connect('192.168.169.30')
+keithley = cxndmm.keithley_2100_dmm()
 keithley.select_device()
 
-cxnpulser = yield connectAsync('192.168.169.29')
-pulser = cxnpulser.pulser()
-#Initially switch off the TTL pulse.
-pulser.switch_manual("Thermometer", False)
-
+pulser.switch_manual('Thermometer', False)
 run_time = time.strftime("%d%m%Y_%H%M")
 initial_time = time.time()
 #BNC 526 is at Cold Finger
-filedirectory_526 ='c:/data_resonator_voltage/526(Cold Finger)_keithley_DMM_'+run_time+'.csv'
+filedirectory_526 = '/home/resonator/Desktop/Resonator_Voltage/526(Cold Finger)_'+run_time+'_keithley_DMM.csv'
 #BNC 529 is inside the heat shield
-filedirectory_529 ='c:/data_resonator_voltage/529(Inside Heat Shield)_keithley_DMM_'+run_time+'.csv'
+filedirectory_529 = '/home/resonator/Desktop/Resonator_Voltage/529(Inside Heat Shield)_'+run_time+'_keithley_DMM.csv'
 
 file_526 = open(filedirectory_526,"wb")
 fcsv_526 = csv.writer(file_526,lineterminator="\n")
-fcsv_526.writerow(["ElapsedTime_(minutes)", "CurrentTime(H:M)", "Voltage(V)", "Temperature(K)"])
+fcsv_526.writerow(["ElapsedTime(minutes)", "CurrentTime(H:M)", "Voltage(V)", "Temperature(K)"])
 file_526.close()
 
 file_529 = open(filedirectory_529,"wb")
 fcsv_529 = csv.writer(file_529,lineterminator="\n")
-fcsv_529.writerow(["ElapsedTime_(minutes)", "CurrentTime(H:M)", "Voltage(V)", "Temperature(K)"])
+fcsv_529.writerow(["ElapsedTime(minutes)", "CurrentTime(H:M)", "Voltage(V)", "Temperature(K)"])
 file_529.close()
-
-vc = VC()
-
-# Inverter's 1A is connected to COM1(BNC 526)'s logic, and 1Y is connected to COM2(BNC 529)'s logic,
-# so when we want to measure 526 -> pulse.switch_manual("Thermometer", False): IN1 = LOW / IN2 = HIGH
-# and when we measure 529 -> pulse.switch_manual("Thermometer", True): IN1 = HIGH / IN2 = LOW
-# Switch is normally closed, so when we apply the HIGH logic, it will measure the voltage. 
     
+vc = VC()
 while(1):
     file_526=open(filedirectory_526,"ab")
     fcsv_526=csv.writer(file_526,lineterminator="\n")
@@ -45,16 +37,15 @@ while(1):
     elapsed_time_526 = (time.time() - initial_time)/60
     fcsv_526.writerow([elapsed_time_526, time.strftime("%H"+":"+"%M"), voltage, tempK])
     file_526.close()
-    pulser.switch_manual("Thermometer", True)
+    pulser.switch_manual('Thermometer', True)
     time.sleep(30)
     
     file_529 = open(filedirectory_529,"ab")
     fcsv_529 = csv.writer(file_529,lineterminator="\n")
-    pulser.Start_Single()
     voltage = keithley.get_dc_volts()
     tempK = vc.conversion(voltage)
     elapsed_time_529 = (time.time() - initial_time)/60
     fcsv_529.writerow([elapsed_time_529, time.strftime("%H"+":"+"%M"), voltage, tempK])
     file_529.close()
-    pulser.switch_manual("Thermometer", False)
+    pulser.switch_manual('Thermometer', False)
     time.sleep(30)
