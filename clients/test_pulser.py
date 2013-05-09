043f76a7478fdc3b759c23582eb16d1dbc402f46
ryohmasuda <ryohmasuda@berkeley.edu>
2013-05-09 10:07:44 -0700

    Update for testing pulser on Linux

--- clients/test_pulser.py
+++ clients/test_pulser.py
@@ -1,14 +1,23 @@
 import labrad
 import time
 
-cxn = labrad.connectAsync('192.168.169.29')
-pulse = cxn.pulser()
+# Test the Pulser on Linux
+cxn = labrad.connect()
+pulser = cxn.pulser()
 
-count = 0 
+count = 0
 while (count <= 10):
-  pulse.switch_manual("11",True)
-  sleep(5)
-  pulse.switch_manual("11",False)
-  sleep(5)
-  count += 1
-  
+    time.sleep(5)
+    pulser.switch_manual("422", False)
+    time.sleep(3)
+    pulser.switch_manual("375", False)
+    time.sleep(3)
+    pulser.switch_manual("OvenLaser", False)
+    time.sleep(3)
+    pulser.switch_manual("Thermometer", False)
+    time.sleep(3)
+    pulser.switch_manual("422", True)
+    pulser.switch_manual("375", True)
+    pulser.switch_manual("OvenLaser", True)
+    pulser.switch_manual("Thermometer", True)
+    count += 1
